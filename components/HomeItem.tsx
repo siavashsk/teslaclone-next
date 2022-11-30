0872ed410a59546628fe4d6580f9fbdf642228cf
siavash <siavash.khani200@gmail.com>
2022-11-30 21:24:52 +0330

    feat(HomeItem.tsx): component is now reusable

--- components/HomeItem.tsx
+++ components/HomeItem.tsx
@@ -1,36 +1,39 @@
 import React from "react";
 
-const HomeItem = () => {
-  return (
-    <>
-      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
-        <img
-          src="/model3.jfif"
-          alt="model3"
-          className="w-full h-[100vh] object-cover"
-        />
-      </div>
-      <div className="flex justify-center items-center flex-col pt-28">
-        <h1 className="text-4xl font-semibold">Model 3</h1>
-        <p
-          className="underline font-normal pt-2 text-gray
-          text-sm"
-        >
-          Schedule a Test Drive
-        </p>
+interface T {
+  id: number;
+  name: string;
+  subText: string;
+  image: string;
+}
 
-        <div className="absolute bottom-28">
-          <div className="sm:flex justify-center gap-6">
-            <button className="w-60 h-9 text-sm rounded-md text-center bg-gray-dark text-white font-semibold">
-              Custom Order
-            </button>
-            <button className="w-60 h-9 text-sm rounded-md text-center bg-[#FFFFFFA6] text-gray font-semibold">
-              Existing Inventory
-            </button>
-          </div>
+const HomeItem = ({ name, subText, image }: T) => {
+  return (
+    <div className="h-[100vh]">
+      <div className="absolute -z-10 h-[95vh] w-screen">
+        <img src={image} alt={name} className="w-full h-[100vh] object-cover" />
+      </div>
+      <div className="h-[86%] flex flex-col justify-between items-center pt-28">
+        <div className="flex flex-col">
+          <h1 className="text-4xl font-semibold text-[#222]">{name}</h1>
+          <p
+            className="underline font-normal pt-2 text-gray cursor-pointer
+          text-sm"
+          >
+            {subText}
+          </p>
+        </div>
+
+        <div className="sm:flex justify-center gap-6">
+          <button className="w-60 h-9 text-sm rounded-md text-center bg-gray-dark text-white font-semibold">
+            Custom Order
+          </button>
+          <button className="w-60 h-9 text-sm rounded-md text-center bg-[#FFFFFFA6] text-gray font-semibold">
+            Existing Inventory
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
