b223abe985e192fbed4a0fe02f0d9a0c860db39f
siavash <siavash.khani200@gmail.com>
2022-11-30 21:24:52 +0330

    feat(HomeItem.tsx): made the component reusable

--- data/index.tsx
+++ data/index.tsx
@@ -1,8 +1,47 @@
 export const navItems = [
-    { id: 1, name: "Model S" },
-    { id: 2, name: "Model 3" },
-    { id: 3, name: "Model X" },
-    { id: 4, name: "Model Y" },
-    { id: 5, name: "Solar Roof" },
-    { id: 6, name: "Solar Panels" },
+  { id: 1, name: "Model S" },
+  { id: 2, name: "Model 3" },
+  { id: 3, name: "Model X" },
+  { id: 4, name: "Model Y" },
+  { id: 5, name: "Solar Roof" },
+  { id: 6, name: "Solar Panels" },
+];
+
+export const HomeItems = [
+  {
+    id: 1,
+    name: "Model 3",
+    subText: "Schedule a Test Drive",
+    image: "/model3.jfif",
+  },
+  {
+    id: 2,
+    name: "Model Y",
+    subText: "Schedule a Test Drive",
+    image: "/modely.jfif",
+  },
+  {
+    id: 3,
+    name: "Model Y",
+    subText: "Schedule a Test Drive",
+    image: "/models.jfif",
+  },
+  {
+    id: 4,
+    name: "Model X",
+    subText: "Schedule a Test Drive",
+    image: "/modelx.jfif",
+  },
+  {
+    id: 5,
+    name: "Solar Panels",
+    subText: "Lowest Cost Solar Panels in America",
+    image: "/solarpanel.jfif",
+  },
+  {
+    id: 6,
+    name: "Solar Roof",
+    subText: "Produce Clean Energy From Your Roof",
+    image: "/solarroof.webp",
+  },
 ];
